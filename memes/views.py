a8fba56d576808f9c2bbc51fca3af2efbb5f6b29
irene-tiamo <87821949+irene-tiamo@users.noreply.github.com>
2021-10-24 23:28:51 +0800

    update

--- memes/views.py
+++ memes/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from memes.models import Photo
 
 def home(request):
-    return render(request, 'memes.html',{
-    })
+    photos=Photo.objects.all()
+    context={'photos':photos}
+    return render(request, 'memes.html', context)
 def upload(request):
     return render(request, 'upload.html',{
     })
