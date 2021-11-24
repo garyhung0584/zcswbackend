0de6fabc96cb59366462fcf3250132dc0e95ddb7
irene-tiamo <87821949+irene-tiamo@users.noreply.github.com>
2021-11-24 11:23:11 +0800

    update

--- memes/views.py
+++ memes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views import generic
 from memes.models import Photo
@@ -6,8 +6,15 @@ from memes.forms import UploadModelForm
 
 def home(request):
     photos = Photo.objects.all()
-    #form=UploadModelForm
-    #context={'form':form}
+    form=UploadModelForm()
+    if request.method == "POST":
+        form = UploadModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/memes')
+    context={
+        'photos':photos,
+        'form':form}
     return render(request, 'memes.html', { 'photos' : photos })
 def upload(request):
     return render(request, 'upload.html',{
