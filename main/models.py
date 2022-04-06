52749ce8d676f501790aad404b0390e8b5cee12a
unknown <jamie44566@gmail.com>
2022-04-06 16:29:34 +0800

    image crop

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Member(models.Model):
     user = models.OneToOneField(User, null = True, on_delete = models.CASCADE)
@@ -9,6 +10,16 @@ class Member(models.Model):
     likes = models.IntegerField(default = 0, null = True)
     profile_pic = models.ImageField(default = "default.png", blank = True, null = True, upload_to='member/')
     date_create = models.DateTimeField(auto_now_add = True, null = True)
+
+    def save(self,*args,**kwargs):
+        super().save(*args,**kwargs)
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
+
     '''
     def __str__(self):
         if self.user:
